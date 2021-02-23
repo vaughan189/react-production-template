26cab62a367f27d988dbc729ae45eae4ac11238f
Vaughan Po <vaughan188@gmail.com>
2021-02-23 16:51:09 +0530

    baseline

---
+++ src/styles/muiTheme.js
@@ -0,0 +1,18 @@
+/* eslint-disable import/no-anonymous-default-export */
+export default {
+    palette: {
+      type: 'light',
+      primary: {
+        main: "#040f3d",
+      },
+      secondary: {
+        main: "#757575",
+      },
+    },
+    typography: {
+      button: {
+        // textTransform: "none",   
+        //Using this will stop tranforming all button texts to UPPERCASE
+      },
+    }
+  }
