9a0c2760ad39131281ae07197743736b916ce0f7
Vaughan Po <vaughan188@gmail.com>
2021-02-24 12:04:30 +0530

    ARCHITECTURE—DEVELOP/ADD FEATURES (with EXTERNAL LIBRARIES)

--- src/App.js
+++ src/App.js
@@ -1,24 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+// Handle console logs
+import "utils/dropConsole";
+// Styles
+import "fontsource-roboto";
+// ROUTER
+import { BrowserRouter } from "react-router-dom";
+import { RouterConfig } from "navigation/RouterConfig";
+// MUI Theme
+import { ThemeProvider, Button, createMuiTheme } from "@material-ui/core";
+import { ThemeSwitch } from "components/ThemeSwitch";
+import { dark, light } from "styles/muiTheme";
+import "./App.css";
+import { ProvideAuth } from "navigation/Auth/ProvideAuth";
+// Redux
+import { Provider } from "react-redux";
+import {store} from "redux/store";
 
 function App() {
+  const [darkState, setDarkState] = useState(false);
+  const handleThemeChange = () => {
+    setDarkState(!darkState);
+    console.log("theme=", darkState ? "dark" : "light");
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div>
+        <Provider store={store}>
+          <ThemeProvider theme={darkState ? dark() : light()}>
+            <ThemeSwitch
+              darkState={darkState}
+              handleThemeChange={handleThemeChange}
+            />
+            <ProvideAuth>
+              <BrowserRouter>
+                <RouterConfig />
+              </BrowserRouter>
+            </ProvideAuth>
+          </ThemeProvider>
+        </Provider>
+      </div>
+    </>
   );
 }
 
---
+++ src/redux/store.js
@@ -0,0 +1,19 @@
+import { applyMiddleware, compose, createStore } from "redux";
+import thunk from "redux-thunk";
+import { rootReducer } from "./reducers";
+
+// Array of all middlewares to be applied.
+const middlewares = [thunk];
+
+// devtools for debugging in dev environment.
+const devTools =
+  // eslint-disable-next-line no-undef
+  process.env.NODE_ENV !== "production" && window.__REDUX_DEVTOOLS_EXTENSION__
+    ? window.__REDUX_DEVTOOLS_EXTENSION__ &&
+      window.__REDUX_DEVTOOLS_EXTENSION__()
+    : (a) => a;
+
+export const store = createStore(
+  rootReducer,
+  compose(applyMiddleware(...middlewares), devTools)
+);
