aeb4e0867ba68024ebd8fe87d71936756f1fdc55
Vaughan Po <vaughan188@gmail.com>
2021-08-08 15:36:08 +0530

    jest, react testing library setup

---
+++ test/Unit/App.spec.js
@@ -0,0 +1,15 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import { Provider } from 'react-redux';
+// import store from '../../src/state/Store';
+// import App from '../../src/theme';
+
+it('renders without crashing', () => {
+  // const div = document.createElement('div');
+  // ReactDOM.render(
+  //   <Provider store={store}>
+  //     <App />
+  //   </Provider>,
+  //   div
+  // );
+});
---
+++ test/setup.js
@@ -0,0 +1,5 @@
+// cleanup helper
+import '@testing-library/react/cleanup-after-each';
+// custom matchers for jest
+// learn more: https://github.com/testing-library/jest-dom
+import '@testing-library/jest-dom/extend-expect';
